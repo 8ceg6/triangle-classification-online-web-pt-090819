739af93182c95db47424fe05094ee159ff5d47ab
cedric e goddard <8ceg6@comcast.net>
2019-10-19 17:31:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,35 +1,37 @@
 require_relative '../lib/triangle'
 class Triangle
- attr_accessor :a, :b, :c 
+
+
+# attr_accessor :a, :b, :c 
  
-  def initialize
-      @a = a 
-      @b = b 
-      @d = c
-  end 
-  
-  def kind(a, b, c) 
-     
-  if ((a == b) && (a == c) && (b == c))
-    return :equilateral
-  elsif ((a == b) || (a == c) || (b == c))
-    return :isosceles
-  else
-    return :scalene
-  end
-  end
-  
-#   def triangle_type1 
-#       a.positive? && b.positive? && c.positive?
+#   def initialize
+#       @a = a 
+#       @b = b 
+#       @d = c
 #   end 
   
-#   def triangle_type2
-#       a + b > c && a + c > b && b + c > a 
+#   def kind(a, b, c) 
+     
+#   if ((a == b) && (a == c) && (b == c))
+#     return :equilateral
+#   elsif ((a == b) || (a == c) || (b == c))
+#     return :isosceles
+#   else
+#     return :scalene
 #   end
-end
+#   end
+  
+# #   def triangle_type1 
+# #       a.positive? && b.positive? && c.positive?
+# #   end 
+  
+# #   def triangle_type2
+# #       a + b > c && a + c > b && b + c > a 
+# #   end
+# end
 
-class TriangleError < StandardError
-end 
+# class TriangleError < StandardError
+# end 
 
 
 
