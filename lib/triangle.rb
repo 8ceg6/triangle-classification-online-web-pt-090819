d78f60da107d4ff20c2d4d7ed1cab481edd5ca28
cedric e goddard <8ceg6@comcast.net>
2019-10-19 17:31:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,45 @@
 require_relative '../lib/triangle'
 class Triangle
 
+attr_accessor :a, :b, :c
+
+ def initalize(a, b, c)
+  @a = a
+  @b = b
+  @c = c
+ end
+
+
+ def kind
+     validate_triangle
+     if a_side == b_side && b_side == c_side
+       :equilateral
+     elsif a_side == b_side || b_side == c_side || c_side == a_side
+       :isosceles
+     else
+       :scalene
+     end
+   end
+
+
+    def validate_triangle
+      valid_triangle = [
+        (a_side + b_side > c_side),
+        (a_side + c_side > b_side),
+        (b_side + c_side > a_side)
+      ]
+      [a_side, b_side, c_side].each do |side|
+        valid_triangle << false if side <= 0
+        raise TriangleError if valid_triangle.include?(false)
+      end
+    end
+
+
+
+class TriangleError < StandardError
+end
+
+end
 
 # attr_accessor :a, :b, :c 
  
